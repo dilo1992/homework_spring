91baeda15671219b1e3011d8e4e0fe67bee6b3a2
dmitrylobov <dilo1992.edu@gmail.com>
2023-01-07 21:16:57 +0300

    Initial commit

---
+++ src/main/java/by/lobov/homework_spring/HomeworkSpringApplication.java
@@ -0,0 +1,13 @@
+package by.lobov.homework_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/lobov/homework_spring/HomeworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobov.homework_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
