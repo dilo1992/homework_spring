2b0c6d7a97ee9568c2d7cb3dbb89faafbd73ce5b
dmitrylobov <dilo1992.edu@gmail.com>
2023-02-20 00:47:26 +0300

    Homework, lesson 40 (thymeleaf) with Spring Data JPA (after teacher changes)

--- src/main/java/by/lobov/service/HW37/StudentService.java
+++ src/main/java/by/lobov/service/HW37/StudentService.java
@@ -1,5 +1,7 @@
 package by.lobov.service.HW37;
 
+import by.lobov.entity.HW37.Address;
+import by.lobov.entity.HW37.Course;
 import by.lobov.entity.HW37.Student;
 import by.lobov.repository.HW37.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,33 @@ public class StudentService {
         } else if (student.getAddress() == null) {
             throw new NullPointerException("Student cannot not have address");
         }
+//        Student firstStudent = new Student();
+//        firstStudent.setName("Dima");
+//
+//        Address firstAddress = new Address();
+//        firstAddress.setAddress("Minsk");
+//
+//        firstStudent.setAddress(firstAddress);
+//
+//        Student secondStudent = new Student();
+//        secondStudent.setName("Sasha");
+//
+//        Address secondAddress = new Address();
+//        secondAddress.setAddress("Brest");
+//
+//        secondStudent.setAddress(secondAddress);
+//
+//        Course course = new Course();
+//        course.setCountOfStudents(30);
+//        course.setSpecialization("IT");
+//
+//        course.addStudentsInCourse(firstStudent);
+//
+//        course.addStudentsInCourse(secondStudent);
+//        studentRepository.save(firstStudent);
+//        studentRepository.save(secondStudent);
+//        log.info("Saved firstStudent: {}", firstStudent);
+//        log.info("Saved secondStudent: {}", secondStudent);
         return studentRepository.save(student);
     }
 }
